918a7db3808d84a6d85016ae4c3647939750e648
ysg00 <allanyu1130@gmail.com>
2020-03-18 18:26:16 -0500

    Updates

---
+++ precache-manifest.dc4c26eb217501ed87e2edd005316821.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905a86c65b83e2bfa2f24010070d80d3",
+    "url": "/ysg00/covid-19/index.html"
+  },
+  {
+    "revision": "e783424a0f8152473bba",
+    "url": "/ysg00/covid-19/static/css/2.09cc9c4c.chunk.css"
+  },
+  {
+    "revision": "c2a4a46475be12da482a",
+    "url": "/ysg00/covid-19/static/css/main.5481cf95.chunk.css"
+  },
+  {
+    "revision": "e783424a0f8152473bba",
+    "url": "/ysg00/covid-19/static/js/2.ab7fec3e.chunk.js"
+  },
+  {
+    "revision": "94f6b4b928c527cf45cc0dbd33ec89f2",
+    "url": "/ysg00/covid-19/static/js/2.ab7fec3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a4a46475be12da482a",
+    "url": "/ysg00/covid-19/static/js/main.bb2990a2.chunk.js"
+  },
+  {
+    "revision": "a2accaf9ae055f083c13",
+    "url": "/ysg00/covid-19/static/js/runtime-main.927f1e75.js"
+  }
+]);
